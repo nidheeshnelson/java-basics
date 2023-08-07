4f6c7c6abe1e70229b7d06767aa9d9ee78c0f996
nidheeshnelson <nidheeshnelson10@gmail.com>
2023-08-07 13:32:04 +0530

    Nidheesh Nelson

---
+++ WrapperClass/src/Character1/Character1.java
@@ -0,0 +1,5 @@
+package Character1;
+
+public class Character1 {
+
+}
---
+++ WrapperClass/src/IntegerType/Integer1.java
@@ -0,0 +1,18 @@
+package IntegerType;
+
+public class Integer1 {
+	static void value()
+	{
+	int a = 2;
+	Integer intobj = new Integer(a);
+	
+		System.out.println(intobj);
+		int b = intobj;
+		System.out.println(b);
+	}
+	public static void main(String[] args) {
+		value();
+	}
+	
+
+}
