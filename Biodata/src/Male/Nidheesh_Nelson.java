b2aab2032742c782d35d3bd7b8ff2485d78388d1
nidheeshnelson <nidheeshnelson10@gmail.com>
2023-08-07 13:14:30 +0530

    first init

---
+++ Biodata/src/Male/Nidheesh_Nelson.java
@@ -0,0 +1,40 @@
+package Male;
+
+public class Nidheesh_Nelson {
+
+	public static void main(String[] args) {
+		System.out.println(" _________________________________________________________________________________________________________________");
+		System.out.println("|                                                 --Biodata--                                                     |");
+		System.out.println("| # Name                     : Nidheesh Nelson                                                                    |"); 
+		System.out.println("| # email                    : nidheeshnelson10@gmail.com                                                         |");
+		System.out.println("| # Phone Number             : +91 8086906582, 7012024782                                                         |");
+		System.out.println("| # Gender                   : Male                                                                               |");
+		System.out.println("| # Age                      : 31                                                                                 |");
+		System.out.println("| # Marital Status           : Married                                                                            |");
+		System.out.println("| # Spouse                   : Braji U M                                                                           |");
+		System.out.println("| # Place                    : Nellimoodu, Neyyattinkara, Trivandrum                                              |");
+		System.out.println("| # Address                  : Nidhi Nivas,                                                                       |");
+		System.out.println("|                              Thongalvila, Nellimoodu PO                                                         |");
+		System.out.println("|                              Thiruvananthapuram, Kerala, India                                                  |");
+		System.out.println("|                              Pin: 695524                                                                        |");
+		System.out.println("| # Religian                 : Christian                                                                          |");
+		System.out.println("| # Cast                     : Nadar                                                                              |");
+		System.out.println("| # Occupation               : Service Engineer, Aleta Software Labs Pvt Ltd                                      |");
+		System.out.println("| # Educatioin qualificatiion: 1. MBA -Finance and HR                           (Kerala University)     2014-2018 |");
+		System.out.println("|                              2. MBA -Logistics and supply chain management    (Bharathiar University) 2015-2017 |");
+		System.out.println("|                              3. BE  -Electronics and Communication Engineering(Anna University)       2010-2014 |");
+		System.out.println("|                              4. HSE -Biolagy Science                          (Kerala Board)          2008-2010 |");
+		System.out.println("|                              5. SSLC-General                                  (Kerala Board               -2008 |");
+		System.out.println("| # Father's Name            : Nelson C                                                                           |");
+		System.out.println("| # Father's Occupation      : Shop Keeper                                                                        |");
+		System.out.println("| # Mother's Name            : Geetha O                                                                           |");
+		System.out.println("| # Mother's Occupation      : House wife                                                                         |");
+		System.out.println("| # Brother's Name           : Nidhin Nelson                                                                      |");
+		System.out.println("| # Brother's Occupation     : Assistant Manager, CFCICI                                                          |");
+		System.out.println("|_________________________________________________________________________________________________________________|");
+		
+		
+		
+	}
+	
+}
